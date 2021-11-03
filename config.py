5e8fe71dcc1feb0d1ef2c426683bb837bc63a8c9
stygian2a <adrien.nivaggioli@polytechnique.edu>
2021-11-03 12:42:53 +0100

    Update config.py
    Setup for our dataset config.
    im_size=256
    fileformat=png

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ def get_config(FLAGS, is_train):
   config.batch_size = 1  # The current implementation only supports a batch size equal to unity!
 
   if is_train:
-    config.im_size = 500 # The width and height should be equal. Upper bound of the input image size is limited to the GPU memory.
+    config.im_size = 256 # The width and height should be equal. Upper bound of the input image size is limited to the GPU memory.
     config.lr = 1e-4
     config.iteration = 10e6
     
@@ -23,15 +23,15 @@ def get_config(FLAGS, is_train):
     config.SavingInterval  = 500
 
     config.image_options = {'resize': False, 'resize_size': config.im_size, 'crop': True, 'flip': True, 'rotate_stepwise': True}
-    config.fileformat = 'tif'
+    config.fileformat = 'png'
     
   else:
     config.batch_size = 1
-    config.im_size = 500 # The width and height should be equal. Upper bound of the input image size is limited to the GPU memory.
+    config.im_size = 256 # The width and height should be equal. Upper bound of the input image size is limited to the GPU memory.
 
     config.ReportInterval  = None
     config.SavingInterval  = None
     config.image_options = {'resize': False, 'resize_size': config.im_size, 'crop': True, 'flip': False, 'rotate_stepwise': False}
-    config.fileformat = 'tif'
+    config.fileformat = 'png'
 
   return config
